2a284a3a6e83c15eb02baaea0c78f0ca4496032f
mizuki-yamada <yamada.mizuki.0730@gmail.com>
2023-07-08 20:41:20 +0900

    refactor to TypeScript

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import { Practice1 } from './practices/Practice1.tsx';
-import { Practice2 } from './practices/Practice2.tsx';
+import { Practice1 } from './practices/Practice1';
+import { Practice2 } from './practices/Practice2';
 
 function App() {
   return (
